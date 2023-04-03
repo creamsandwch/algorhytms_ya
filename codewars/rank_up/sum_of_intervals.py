fd8270368364cce6fcd3a23a5c355af66085282a
creamsandwch <neuronrus1996@gmail.com>
2023-04-03 12:29:49 +0500

    updates

--- codewars/rank_up/sum_of_intervals.py
+++ codewars/rank_up/sum_of_intervals.py
@@ -2,12 +2,24 @@
 
 def do_intervals_overlap(interval_a, interval_b):
     if (
-        interval_a[0] > interval_b[-1]
+        interval_a[0] <= interval_b[-1]
         or
-        interval_a[-1] < interval_b[0]
+        interval_a[-1] >= interval_b[0]
     ):
         return False
     return True
+
+
+def get_interval_length(interval):
+    start, end = interval[0], interval[-1]
+    if start >= 0 and end > 0:
+        return end - start
+    elif start <= 0 and end > 0:
+        return abs(start) + end
+    elif start < 0 and end < 0:
+        return abs(start) - abs(end)
+    else:
+        return abs(end) + start
 
 
 def sum_of_intervals(intervals: list):
@@ -16,11 +28,12 @@ def sum_of_intervals(intervals: list):
 
     for i in range(len(intervals)):
         for interval in intervals:
-            if (
-                intervals[i][0] > interval[0]
-                and intervals[i][-1] < interval[-1]
-            ):
-                to_remove.append(intervals[i])
+            if intervals.index(interval) != i:
+                if (
+                    intervals[i][0] >= interval[0]
+                    and intervals[i][-1] <= interval[-1]
+                ):
+                    to_remove.append(intervals[i])
 
     clean_intervals = []
 
@@ -30,32 +43,37 @@ def sum_of_intervals(intervals: list):
 
     result = []
 
+    print(f'to_remove = {to_remove}, clean_intervals = {clean_intervals}')
+
+    for interval in clean_intervals:
+        print(f'interval {interval} length = {get_interval_length(interval)}')
+
     for i in range(1, len(clean_intervals)):
         check = do_intervals_overlap(
             interval_a=clean_intervals[i - 1],
             interval_b=clean_intervals[i]
         )
         if check:
-            result.append((abs(clean_intervals[i - 1][0] - clean_intervals[i][0]), abs(clean_intervals[i - 1][-1] - clean_intervals[i - 1][-1])))
-        else:
-            result.append(abs(clean_intervals[i][-1] - clean_intervals[i][0]))
+            if clean_intervals[i - 1][0] <= clean_intervals[i][0]:
+                clean_intervals[i][0] = clean_intervals[i - 1][0]
+            elif clean_intervals[i - 1][-1] >= clean_intervals[i][-1]:
+                clean_intervals[i][-1] = clean_intervals[i - 1][-1]
 
-    output = 0
+        print(f'clean_intervals[i] = {clean_intervals[i]}')
+        result.append(clean_intervals[i][-1] - clean_intervals[i][0])
 
-    for elem in result:
-        if type(elem) == int:
-            output += elem
-        else:
-            output += abs(elem[-1] - elem[0])
+    print(f'clean_intervals={clean_intervals}, result={result}')
 
-    return output
+    return sum(result)
 
 
 def main():
     intervals = [
-        [1, 4],
-        [7, 10],
-        [3, 5]
+        [1, 5],
+        [10, 20],
+        [1, 6],
+        [16, 19],
+        [5, 11]
     ]
 
     return sum_of_intervals(intervals)
